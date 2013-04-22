148d05c6bb139518a1660e04e2d0e31cd9d1955d
Albert Jiao <albert.jiao@yale.edu>
2013-04-22 14:01:42 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AASAWebsite::Application.config.secret_token = 'fd25d68982fd6ab6d19378b9b00f45c65d1e0d78667fb67d3c8bf1fbb90b69c4e9ab17644a63f3bc1e933eaef221c7d34c1db410fc03be6ba7d78525138cb93b'
